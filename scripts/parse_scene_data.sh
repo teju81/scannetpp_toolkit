57d106d2cee21ad616d68eecaf2889cdcbaa0f7e
teju81 <teju81@gmail.com>
2025-01-19 12:08:30 +0530

    Create parse_scene_data.sh

---
+++ scripts/parse_scene_data.sh
@@ -0,0 +1,2 @@
+source ~/virtual_envs/scannetpp_env/scannetpp_env/bin/activate
+python parse_scannetpp.py --data_base /home/raviteja/code/datasets/scannetpp/data/0a5c013435 --output_path /home/raviteja/code/datasets/scannetpp/parsed
