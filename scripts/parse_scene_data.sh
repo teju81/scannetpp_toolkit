f71e19d8a9cbbe359b895b98bd29d9856e8d9a08
teju81 <teju81@gmail.com>
2025-01-20 12:57:31 +0530

    Delete scripts/parse_scene_data.sh

--- scripts/parse_scene_data.sh
+++
@@ -1,2 +0,0 @@
-source ~/virtual_envs/scannetpp_env/scannetpp_env/bin/activate
-python parse_scannetpp.py --data_base /home/raviteja/code/datasets/scannetpp/data/0a5c013435 --output_path /home/raviteja/code/datasets/scannetpp/parsed
